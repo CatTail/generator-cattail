a3dfbbb9d2e84376dba1899bd2a238192ea32f50
zhongchiyu <zhongchiyu@gmail.com>
2014-09-19 01:08:51 +0800

    feat: combine org and repo info

--- node/index.js
+++ node/index.js
@@ -6,64 +6,62 @@ var shell = require('shelljs');
 
 
 var CattailGenerator = yeoman.generators.NamedBase.extend({
-  init: function () {
-    this.log('You called the cattail subgenerator with the argument ' + this.name + '.');
-  },
+    init: function () {
+        this.log('You called the cattail subgenerator with the argument ' + this.name + '.');
+    },
 
-  ask: function() {
-    var done = this.async();
-    this.prompt([{
-      type: 'input',
-      name: 'repo',
-      message: 'Repo',
-      default: this.name
-    }, {
-      type: 'input',
-      name: 'description',
-      message: 'Description'
-    }, {
-      type: 'input',
-      name: 'org',
-      message: 'Orgnization name',
-      default: 'CatTail'
-    }], function(answers) {
-      this.data = answers;
-      done();
-    }.bind(this));
-  },
+    ask: function() {
+        var done = this.async();
+        this.prompt([{
+            type: 'input',
+            name: 'orgrepo',
+            message: 'Org/Repo',
+            default: 'CatTail/' + this.name
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'Description'
+        }], function(answers) {
+            var orgrepo = answers.orgrepo.split('/');
+            answers.org = orgrepo[0];
+            answers.repo = orgrepo[1];
+            this.data = answers;
+            done();
+        }.bind(this));
+    },
 
-  data: function() {
-    _.merge(this.data, {
-      name: this.name,
-      year: new Date().getFullYear(),
-    });
-  },
+    data: function() {
+        _.merge(this.data, {
+            name: this.name,
+            year: new Date().getFullYear(),
+        });
+    },
 
-  node: function () {
-    this.mkdir(this.data.name);
-    process.chdir(this.data.name);
+    node: function () {
+        this.mkdir(this.data.name);
+        process.chdir(this.data.name);
 
-    this.copy('History.md', 'History.md');
-    this.copy('_travis.yml', '.travis.yml');
-    this.copy('index.js', 'index.js');
+        this.copy('History.md', 'History.md');
+        this.copy('_travis.yml', '.travis.yml');
+        this.copy('index.js', 'index.js');
 
-    this.template('LICENSE', 'LICENSE', this.data);
-    this.template('Readme.md', 'Readme.md', this.data);
-    this.template('_package.json', 'package.json', this.data);
+        this.template('LICENSE', 'LICENSE', this.data);
+        this.template('Readme.md', 'Readme.md', this.data);
+        this.template('_package.json', 'package.json', this.data);
 
-    this.mkdir('test');
-  },
+        this.mkdir('test');
+    },
 
-  git: function() {
-    shell.exec('git init');
-    shell.exec(util.format('git remote add origin git@github.com:%s/%s.git',
-                           this.data.org, this.data.repo));
-    this.copy('_gitignore', '.gitignore');
-  },
+    git: function() {
+        shell.exec('git init');
+        shell.exec(util.format('git remote add origin git@github.com:%s/%s.git',
+                               this.data.org, this.data.repo));
+                               this.copy('_gitignore', '.gitignore');
+    },
 
-  deps: function() {
-    this.installDependencies();
-  }
+    deps: function() {
+        this.installDependencies();
+    }
 
 });
 
