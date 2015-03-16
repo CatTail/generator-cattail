1a55ad4f7341a561155b26e73b52a3385cd41893
zhongchiyu <zhongchiyu@gmail.com>
2015-03-16 18:02:19 +0800

    Create fixtures directory

--- node/index.js
+++ node/index.js
@@ -56,6 +56,7 @@ var CattailGenerator = yeoman.generators.NamedBase.extend({
         this.template('_package.json', 'package.json', this.data);
 
         this.mkdir('test');
+        this.mkdir('test/fixtures');
     },
 
     git: function() {
