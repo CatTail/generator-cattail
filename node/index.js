84f61a84d493b27f4bd3b673a0446f28abd14680
zhongchiyu <zhongchiyu@gmail.com>
2016-05-16 10:24:36 +0800

    upgrade Node.js to use ES6

--- node/index.js
+++ node/index.js
@@ -1,17 +1,17 @@
-'use strict';
-var util = require('util');
-var yeoman = require('yeoman-generator');
-var _ = require('lodash');
-var shell = require('shelljs');
+'use strict'
 
+var util = require('util')
+var yeoman = require('yeoman-generator')
+var _ = require('lodash')
+var shell = require('shelljs')
 
 var CattailGenerator = yeoman.generators.NamedBase.extend({
-    init: function () {
-        this.log('You called the cattail subgenerator with the argument ' + this.name + '.');
+    init() {
+        this.log('You called the cattail subgenerator with the argument ' + this.name + '.')
     },
 
-    ask: function() {
-        var done = this.async();
+    ask() {
+        var done = this.async()
         this.prompt([{
             type: 'input',
             name: 'orgrepo',
@@ -26,58 +26,62 @@ var CattailGenerator = yeoman.generators.NamedBase.extend({
             name: 'publish',
             message: 'Is publish',
             default: true
-        }], function(answers) {
-            var orgrepo = answers.orgrepo.split('/');
-            answers.org = orgrepo[0];
-            answers.repo = orgrepo[1];
-            this.data = answers;
-            done();
-        }.bind(this));
+        }], (answers) => {
+            var orgrepo = answers.orgrepo.split('/')
+            answers.org = orgrepo[0]
+            answers.repo = orgrepo[1]
+            this.data = answers
+            done()
+        })
     },
 
-    data: function() {
+    data() {
         _.merge(this.data, {
             name: this.name,
             year: new Date().getFullYear(),
-        });
+        })
     },
 
-    node: function () {
-        this.mkdir('node-' + this.data.name);
-        process.chdir('node-' + this.data.name);
-
-        this.copy('History.md', 'History.md');
-        this.copy('_travis.yml', '.travis.yml');
-        this.copy('_jshintrc', '.jshintrc');
-        this.copy('index.js', 'index.js');
-
-        this.template('LICENSE', 'LICENSE', this.data);
-        this.template('Readme.md', 'Readme.md', this.data);
-        this.template('_package.json', 'package.json', this.data);
-
-        this.mkdir('test');
-        this.mkdir('test/fixtures');
+    create() {
+        this.mkdir('node-' + this.data.name)
+        process.chdir('node-' + this.data.name)
     },
 
-    git: function() {
-        shell.exec('git init');
+    git() {
+        shell.exec('git init')
         if (this.data.publish) {
             shell.exec(util.format('git remote add origin git@github.com:%s.git',
-                                   this.data.orgrepo));
-            shell.exec(util.format('hub create %s', this.data.orgrepo));
+                                   this.data.orgrepo))
+            shell.exec(util.format('hub create -d "%s" %s',
+                                   this.data.description, this.data.orgrepo))
         }
 
-        this.copy('_gitignore', '.gitignore');
-        this.copy('githooks.json', 'githooks.json');
-        this.directory('githooks', 'githooks');
-
-        shell.exec('git hooks install');
+        shell.exec('git hooks install')
     },
 
-    deps: function() {
-        this.installDependencies();
-    }
+    skeleton() {
+        this.directory('githooks', 'githooks')
+        this.directory('resources', 'resources')
+        this.copy('_babelrc', '.babelrc')
+        this.copy('_editorconfig', '.editorconfig')
+        this.copy('_eslintignore', '.eslintignore')
+        this.copy('_eslintrc.js', '.eslintrc.js')
+        this.copy('_gitignore', '.gitignore')
+        this.template('_package.json', 'package.json', this.data)
+        this.copy('_travis.yml', '.travis.yml')
+        this.copy('History.md', 'History.md')
+        this.copy('index.js', 'index.js')
+        this.template('LICENSE', 'LICENSE', this.data)
+        this.template('Readme.md', 'Readme.md', this.data)
 
-});
+        this.mkdir('src')
+        this.mkdir('src/__tests__')
+    },
 
-module.exports = CattailGenerator;
+    deps() {
+        this.installDependencies()
+    },
+
+})
+
+module.exports = CattailGenerator
